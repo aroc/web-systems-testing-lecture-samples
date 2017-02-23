8564e8c97779104b17254ffbd36cc44e76b0e700
Eric Anderson <eric@ericanderson.ca>
2017-02-22 22:25:29 -0700

    leave tests ready to be completed

--- test/unit/user_test.js
+++ test/unit/user_test.js
@@ -8,20 +8,17 @@ describe('User', function() {
   describe('#getName()', function() {
 
     it('should return the users name', function() {
-      let user = new User('Eric');
-      assert.equal('Eric', user.getName());
+      // todo
     });
 
   });
 
-  // describe('#setName()', function() {
-  //
-  //   it('should set the name on the user', function() {
-  //     let user = new User('Eric');
-  //     user.setName('Alexa');
-  //     assert.equal('Alexa', user.getName());
-  //   });
-  //
-  // });
+  describe('#setName()', function() {
+
+    it('should set the name on the user', function() {
+      // todo
+    });
+
+  });
 
 });
